e98d71c6396386a65027da9c3654e087a151c21b
Slygriyrsk <124233795+Slygriyrsk@users.noreply.github.com>
2023-09-29 21:56:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,53 +3,64 @@ var NumberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < NumberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
+    mixsound(buttonInnerHTML);
 
-    switch (buttonInnerHTML) {
-      case "w":
-        var audio = new Audio("tom-1.mp3");
-        audio.play();
-        break;
+    // Add more cases for other buttonInnerHTML values if needed
+    // case "a":
+    //   var audio = new Audio("tom-2.mp3");
+    //   audio.play();
+    //   break;
 
-      case "a":
-        var audio = new Audio("tom-2.mp3");
-        audio.play();
-        break;
-      case "s":
-        var audio = new Audio("tom-3.mp3");
-        audio.play();
-        break;
-      case "d":
-        var audio = new Audio("tom-4.mp3");
-        audio.play();
-        break;
-
-      case "j":
-        var audio = new Audio("snare.mp3");
-        audio.play();
-        break;
-
-      case "k":
-        var audio = new Audio("crash.mp3");
-        audio.play();
-        break;
-
-      case "l":
-        var audio = new Audio("kick-bass.mp3");
-        audio.play();
-        break;
-
-      // Add more cases for other buttonInnerHTML values if needed
-      // case "a":
-      //   var audio = new Audio("tom-2.mp3");
-      //   audio.play();
-      //   break;
-
-      // Handle other cases here
-
-      default:
-        console.log("Invalid buttonInnerHTML");
-    }
+    // Handle other cases here
   });
+}
+
+// if button us pressed it will make sound
+
+document.addEventListener("keypress", function (event) {
+  /* alert("key was pressed!"); */
+  /*  console.log(event); */
+  mixsound(event.key);
+});
+
+function mixsound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("tom-1.mp3");
+      audio.play();
+      break;
+
+    case "a":
+      var audio = new Audio("tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("tom-4.mp3");
+      audio.play();
+      break;
+
+    case "j":
+      var audio = new Audio("snare.mp3");
+      audio.play();
+      break;
+
+    case "k":
+      var audio = new Audio("crash.mp3");
+      audio.play();
+      break;
+
+    case "l":
+      var audio = new Audio("kick-bass.mp3");
+      audio.play();
+      break;
+
+    default:
+      console.log("Invalid buttonInnerHTML");
+  }
 }
 
 /* var audio = new Audio("../src/mp/tom-1.mp3");
